26caaca7475fdf955d43061e0ba35a621461f6da
ElielClementino <elielclementinodasilva@gmail.com>
2025-03-01 18:37:44 -0300

    Add endpoint for deleting todos

--- src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,20 @@ public class TodoController {
     public ResponseEntity<Object> updateTodo(@PathVariable Long id, @RequestBody @Valid TodoRecordDto todoRecordDto) {
         try {
             List<Todo> todos = todoService.updateTodo(id, todoRecordDto);
+
+            return ResponseEntity.status(HttpStatus.OK).body(todos);
+        } catch(Exception e) {
+            e.printStackTrace();
+            var errResponse = new ErrorResponseDto("An error occurred while processing your solicitation: " + e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errResponse);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTodo(@PathVariable Long id) {
+        try {
+            List<Todo> todos = todoService.deleteTodo(id);
 
             return ResponseEntity.status(HttpStatus.OK).body(todos);
         } catch(Exception e) {
