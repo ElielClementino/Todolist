b981cfa4e68da86964c89ebcb7b6af9a30373725
ElielClementino <elielclementinodasilva@gmail.com>
2025-03-01 18:31:59 -0300

    Add endpoint for updating todos

--- src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,20 @@ public class TodoController {
             List<Todo> todos = todoService.createTodo(todoRecordDto);
 
             return ResponseEntity.status(HttpStatus.CREATED).body(todos);
+        } catch(Exception e) {
+            e.printStackTrace();
+            var errResponse = new ErrorResponseDto("An error occurred while processing your solicitation: " + e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errResponse);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateTodo(@PathVariable Long id, @RequestBody @Valid TodoRecordDto todoRecordDto) {
+        try {
+            List<Todo> todos = todoService.updateTodo(id, todoRecordDto);
+
+            return ResponseEntity.status(HttpStatus.OK).body(todos);
         } catch(Exception e) {
             e.printStackTrace();
             var errResponse = new ErrorResponseDto("An error occurred while processing your solicitation: " + e.getMessage());
