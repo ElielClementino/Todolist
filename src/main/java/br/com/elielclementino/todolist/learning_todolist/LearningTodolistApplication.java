16d3bbfb210e246973303802c1b61cef7d3acf26
Eliel Clementino <elielclementinodasilva@gmail.com>
2025-03-01 17:10:36 -0300

    Initial setup

---
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/LearningTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.elielclementino.todolist.learning_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTodolistApplication.class, args);
+	}
+
+}
