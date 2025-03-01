728c03ea06803be3d4b540c6eb2f884ef097d315
Eliel Clementino <elielclementinodasilva@gmail.com>
2025-03-01 17:24:54 -0300

    Add dotenv dependency for reading .env file

--- src/main/java/br/com/elielclementino/todolist/learning_todolist/LearningTodolistApplication.java
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/LearningTodolistApplication.java
@@ -3,10 +3,15 @@ package br.com.elielclementino.todolist.learning_todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LearningTodolistApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(LearningTodolistApplication.class, args);
 	}
 
