4329f5c8c75e2c7c45c37a333645234066d12085
Eliel Clementino <elielclementinodasilva@gmail.com>
2025-03-01 17:49:01 -0300

    Add method to return all todos already sorted by priority and name

--- src/main/java/br/com/elielclementino/todolist/learning_todolist/service/TodoService.java
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/service/TodoService.java
@@ -1,7 +1,11 @@
 package br.com.elielclementino.todolist.learning_todolist.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import br.com.elielclementino.todolist.learning_todolist.entity.Todo;
 import br.com.elielclementino.todolist.learning_todolist.repository.TodoRepository;
 
 @Service
@@ -10,5 +14,13 @@ public class TodoService {
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> listTodo() {
+        Sort sort = Sort.by("priority").ascending().and(
+            Sort.by("name").ascending()
+        );
+
+        return todoRepository.findAll(sort);
     }
 }
