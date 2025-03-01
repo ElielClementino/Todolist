a327cc4369f1336a0020945b64060d0b86f4b0d6
Eliel Clementino <elielclementinodasilva@gmail.com>
2025-03-01 17:52:04 -0300

    Add endpoint for retriving all todos created

--- src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
+++ src/main/java/br/com/elielclementino/todolist/learning_todolist/controller/TodoController.java
@@ -1,8 +1,15 @@
 package br.com.elielclementino.todolist.learning_todolist.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.elielclementino.todolist.learning_todolist.dtos.ErrorResponseDto;
+import br.com.elielclementino.todolist.learning_todolist.entity.Todo;
 import br.com.elielclementino.todolist.learning_todolist.service.TodoService;
 
 @RestController
@@ -14,4 +21,17 @@ public class TodoController {
         this.todoService = todoService;
     }
     
+    @GetMapping
+    public ResponseEntity<Object> listTodo() {
+        try {
+            List<Todo> todos = todoService.listTodo();
+
+            return ResponseEntity.status(HttpStatus.OK).body(todos);
+        } catch(Exception e) {
+            e.printStackTrace();
+            var errResponse = new ErrorResponseDto("An error occurred while processing your solicitation: " + e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errResponse);
+        }
+    }
 }
